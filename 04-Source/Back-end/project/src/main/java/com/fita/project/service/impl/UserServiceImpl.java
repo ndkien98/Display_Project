05af6032c4fab3d0ee7eeabbcc713503b880812f
Nguyễn Tùng Bái <tungbai121@gmail.com>
2020-06-24 21:07:49 +0700

    Khung quản lý người dùng

---
+++ 04-Source/Back-end/project/src/main/java/com/fita/project/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.fita.project.service.impl;
+
+import com.fita.project.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserServiceImpl implements UserService {
+}
