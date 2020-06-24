569adf4c2ae9067d2c431e2676874fa773d34ec8
Nguyễn Văn Dần <nvdan614086@gmail.com>
2020-06-24 15:44:01 +0700

    cập nhật chức năng quản lý thể loại

--- 04-Source/Back-end/project/src/main/java/com/fita/project/controller/CategoryController.java
+++ 04-Source/Back-end/project/src/main/java/com/fita/project/controller/CategoryController.java
@@ -1,9 +1,44 @@
 package com.fita.project.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fita.project.dto.CategoryDTO;
+import com.fita.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categories/")
 public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    // Lấy ra tất cả các thể loại
+    @GetMapping("get-all")
+    public ResponseEntity<?> getCategories() {
+        return ResponseEntity.ok(categoryService.getCategories());
+    }
+
+    // Lấy ra thể loại theo "id"
+    @GetMapping("get-by-id/{id}")
+    public ResponseEntity<?> getCategory(@PathVariable int id) {
+        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    }
+
+    // Thêm thể loại
+    @PostMapping("add")
+    public ResponseEntity<?> addCategory(@RequestBody CategoryDTO categoryDTO) {
+        return ResponseEntity.ok(categoryService.addCategory(categoryDTO));
+    }
+
+    // Sửa thể loại
+    @PutMapping("edit/{id}")
+    public ResponseEntity<?> editCategory(@PathVariable int id, @RequestBody CategoryDTO categoryDTO) {
+        return ResponseEntity.ok(categoryService.editCategory(id, categoryDTO));
+    }
+
+    // Xoá thể loại
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<?> deleteCategory(@PathVariable int id) {
+        return ResponseEntity.ok(categoryService.deleteCategory(id));
+    }
 }
