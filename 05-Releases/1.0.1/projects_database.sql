7492678c1c6df8c1be0b6e1b4cec36e9a8e657fc
Nguyễn Tùng Bái <tungbai121@gmail.com>
2020-07-01 22:35:02 +0700

    Cap nhat database

--- 05-Releases/1.0.1/projects_database.sql
+++ 05-Releases/1.0.1/projects_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 01, 2020 at 02:21 PM
+-- Generation Time: Jul 01, 2020 at 05:32 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `projects_database`
 --
-CREATE DATABASE IF NOT EXISTS `projects_database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `projects_database`;
 
 -- --------------------------------------------------------
 
@@ -29,12 +27,9 @@ USE `projects_database`;
 -- Table structure for table `categories`
 --
 
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(20) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `category_name` (`category_name`)
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(20) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -43,9 +38,8 @@ CREATE TABLE IF NOT EXISTS `categories` (
 -- Table structure for table `courses`
 --
 
-DROP TABLE IF EXISTS `courses`;
-CREATE TABLE IF NOT EXISTS `courses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
   `subject_code` varchar(10) NOT NULL,
   `subject_group` int(11) NOT NULL,
   `class_code` varchar(50) CHARACTER SET utf8 NOT NULL,
@@ -53,11 +47,7 @@ CREATE TABLE IF NOT EXISTS `courses` (
   `lecturer_code` varchar(10) NOT NULL,
   `created_date` datetime DEFAULT current_timestamp(),
   `created_by` varchar(10) DEFAULT NULL,
-  `last_modified_date` datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `subject_code` (`subject_code`,`subject_group`,`class_code`,`year_semester_id`),
-  KEY `lecturer_code` (`lecturer_code`),
-  KEY `year_semester_id` (`year_semester_id`)
+  `last_modified_date` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -66,14 +56,10 @@ CREATE TABLE IF NOT EXISTS `courses` (
 -- Table structure for table `departments`
 --
 
-DROP TABLE IF EXISTS `departments`;
-CREATE TABLE IF NOT EXISTS `departments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL,
   `department_code` varchar(5) NOT NULL,
-  `department_name` varchar(50) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `department_code` (`department_code`),
-  UNIQUE KEY `department_name` (`department_name`)
+  `department_name` varchar(50) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -82,15 +68,11 @@ CREATE TABLE IF NOT EXISTS `departments` (
 -- Table structure for table `functions`
 --
 
-DROP TABLE IF EXISTS `functions`;
-CREATE TABLE IF NOT EXISTS `functions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `functions` (
+  `id` int(11) NOT NULL,
   `function_name` varchar(50) CHARACTER SET utf8 NOT NULL,
   `action_code` varchar(20) NOT NULL,
-  `function_description` text DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `function_name` (`function_name`),
-  UNIQUE KEY `action_code` (`action_code`)
+  `function_description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -99,14 +81,10 @@ CREATE TABLE IF NOT EXISTS `functions` (
 -- Table structure for table `lecturers`
 --
 
-DROP TABLE IF EXISTS `lecturers`;
-CREATE TABLE IF NOT EXISTS `lecturers` (
+CREATE TABLE `lecturers` (
   `lecturer_code` varchar(10) NOT NULL,
   `role_id` int(11) NOT NULL DEFAULT 2,
-  `department_code` varchar(5) NOT NULL,
-  PRIMARY KEY (`lecturer_code`),
-  KEY `department_code` (`department_code`),
-  KEY `lecturer_code` (`lecturer_code`,`role_id`)
+  `department_code` varchar(5) NOT NULL
 ) ;
 
 -- --------------------------------------------------------
@@ -115,9 +93,8 @@ CREATE TABLE IF NOT EXISTS `lecturers` (
 -- Table structure for table `projects`
 --
 
-DROP TABLE IF EXISTS `projects`;
-CREATE TABLE IF NOT EXISTS `projects` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL,
   `project_name` varchar(200) CHARACTER SET utf8 NOT NULL,
   `project_avatar_url` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
   `short_description` text DEFAULT NULL,
@@ -130,11 +107,7 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `created_date` datetime DEFAULT current_timestamp(),
   `created_by` varchar(10) DEFAULT NULL,
   `last_modified_by` varchar(10) DEFAULT NULL,
-  `last_modified_date` datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `category_id` (`category_id`),
-  KEY `student_code` (`student_code`),
-  KEY `course_id` (`course_id`)
+  `last_modified_date` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -143,15 +116,12 @@ CREATE TABLE IF NOT EXISTS `projects` (
 -- Table structure for table `project_members`
 --
 
-DROP TABLE IF EXISTS `project_members`;
-CREATE TABLE IF NOT EXISTS `project_members` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `project_members` (
+  `id` int(11) NOT NULL,
   `student_code` varchar(10) DEFAULT NULL,
   `full_name` varchar(50) CHARACTER SET utf8 NOT NULL,
   `class_code` varchar(10) DEFAULT NULL,
-  `project_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `project_id` (`project_id`)
+  `project_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -160,12 +130,9 @@ CREATE TABLE IF NOT EXISTS `project_members` (
 -- Table structure for table `roles`
 --
 
-DROP TABLE IF EXISTS `roles`;
-CREATE TABLE IF NOT EXISTS `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(20) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `role_name` (`role_name`)
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `role_name` varchar(20) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -174,15 +141,11 @@ CREATE TABLE IF NOT EXISTS `roles` (
 -- Table structure for table `roles_functions`
 --
 
-DROP TABLE IF EXISTS `roles_functions`;
-CREATE TABLE IF NOT EXISTS `roles_functions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `roles_functions` (
+  `id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
   `function_id` int(11) NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `role_id` (`role_id`,`function_id`),
-  KEY `function_id` (`function_id`)
+  `status` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -191,13 +154,10 @@ CREATE TABLE IF NOT EXISTS `roles_functions` (
 -- Table structure for table `students`
 --
 
-DROP TABLE IF EXISTS `students`;
-CREATE TABLE IF NOT EXISTS `students` (
+CREATE TABLE `students` (
   `student_code` varchar(10) NOT NULL,
   `role_id` int(11) NOT NULL DEFAULT 3,
-  `class_code` varchar(10) NOT NULL,
-  PRIMARY KEY (`student_code`),
-  KEY `student_code` (`student_code`,`role_id`)
+  `class_code` varchar(10) NOT NULL
 ) ;
 
 -- --------------------------------------------------------
@@ -206,16 +166,11 @@ CREATE TABLE IF NOT EXISTS `students` (
 -- Table structure for table `subjects`
 --
 
-DROP TABLE IF EXISTS `subjects`;
-CREATE TABLE IF NOT EXISTS `subjects` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
   `subject_code` varchar(10) NOT NULL,
   `subject_name` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `department_code` varchar(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `subject_code` (`subject_code`),
-  UNIQUE KEY `subject_name` (`subject_name`),
-  KEY `department_code` (`department_code`)
+  `department_code` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -224,9 +179,8 @@ CREATE TABLE IF NOT EXISTS `subjects` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(10) NOT NULL,
   `password` varchar(255) NOT NULL,
   `full_name` varchar(50) CHARACTER SET utf8 NOT NULL,
@@ -238,12 +192,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_avatar_url` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
   `status` int(11) NOT NULL DEFAULT 1,
   `created_date` datetime DEFAULT current_timestamp(),
-  `created_by` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email_address` (`email_address`),
-  UNIQUE KEY `username_2` (`username`,`role_id`),
-  KEY `role_id` (`role_id`)
+  `created_by` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -252,16 +201,190 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `years_semesters`
 --
 
-DROP TABLE IF EXISTS `years_semesters`;
-CREATE TABLE IF NOT EXISTS `years_semesters` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `years_semesters` (
+  `id` int(11) NOT NULL,
   `year` int(11) NOT NULL,
   `semester` smallint(6) NOT NULL,
   `start_date` date DEFAULT NULL,
-  `weeks_number` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `year` (`year`,`semester`)
+  `weeks_number` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `category_name` (`category_name`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `subject_code` (`subject_code`,`subject_group`,`class_code`,`year_semester_id`),
+  ADD KEY `lecturer_code` (`lecturer_code`),
+  ADD KEY `year_semester_id` (`year_semester_id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `department_code` (`department_code`),
+  ADD UNIQUE KEY `department_name` (`department_name`);
+
+--
+-- Indexes for table `functions`
+--
+ALTER TABLE `functions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `function_name` (`function_name`),
+  ADD UNIQUE KEY `action_code` (`action_code`);
+
+--
+-- Indexes for table `lecturers`
+--
+ALTER TABLE `lecturers`
+  ADD PRIMARY KEY (`lecturer_code`),
+  ADD KEY `department_code` (`department_code`),
+  ADD KEY `lecturer_code` (`lecturer_code`,`role_id`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `student_code` (`student_code`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `project_members`
+--
+ALTER TABLE `project_members`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `project_id` (`project_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `role_name` (`role_name`);
+
+--
+-- Indexes for table `roles_functions`
+--
+ALTER TABLE `roles_functions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `role_id` (`role_id`,`function_id`),
+  ADD KEY `function_id` (`function_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_code`),
+  ADD KEY `student_code` (`student_code`,`role_id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `subject_code` (`subject_code`),
+  ADD KEY `department_code` (`department_code`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email_address` (`email_address`),
+  ADD UNIQUE KEY `username_role_id` (`username`,`role_id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Indexes for table `years_semesters`
+--
+ALTER TABLE `years_semesters`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `year` (`year`,`semester`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `functions`
+--
+ALTER TABLE `functions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `project_members`
+--
+ALTER TABLE `project_members`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles_functions`
+--
+ALTER TABLE `roles_functions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `years_semesters`
+--
+ALTER TABLE `years_semesters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
